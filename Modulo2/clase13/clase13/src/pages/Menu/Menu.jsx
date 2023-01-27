7cbc6b52087aa2085f07318f59ef7d68622b2f3a
Fernandojmo <fernando.javier.melo@gmail.com>
2023-01-26 21:28:44 -0300

    ok

--- Modulo2/clase13/clase13/src/pages/Menu/Menu.jsx
+++ Modulo2/clase13/clase13/src/pages/Menu/Menu.jsx
@@ -1,7 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" width='250rem' src="https://images.unsplash.com/photo-1618183479302-1e0aa382c36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
